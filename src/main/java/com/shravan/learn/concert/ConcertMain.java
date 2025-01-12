ca98b013febfae14aebc1568dbb073fa2e1a67ad
shravan.shan <shravan.shan@gmail.com>
2025-01-13 01:02:33 +0530

    concert booking first commit

---
+++ src/main/java/com/shravan/learn/concert/ConcertMain.java
@@ -0,0 +1,22 @@
+package com.shravan.learn.concert;
+
+// ConcertBookingSystem -> Concerts, Venues, Bookings
+//                         addConcert, getConcert, searchConcerts
+//                         bookTickets(User, Concert, Seats) -> Seats.book(), processPayment(Booking), Booking.confirmBooking()
+//                         cancelBooking(bookingId) -> Booking.cancelBooking()
+// User -> name, email
+
+// Booking -> User, Concert, Seats, totalPrice, BookingStatus
+//            calculateTotalPrice(), confirmBooking(), cancelBooking()
+// BookingStatus -> PENDING, CONFIRMED, CANCELLED
+
+// Concert -> artist, Venue, date
+// Venue -> name, Seats,
+// Seat -> seatNumber, SeatType, SeatStatus, price
+//         book(), release()
+// SeatType -> REGULAR, PREMIUM, VIP
+// SeatStatus -> AVAILABLE, BOOKED, RESERVED
+public class ConcertMain {
+    public static void main(String[] args) {
+    }
+}
