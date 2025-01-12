381c349d8a5659d11c8519cc2010d1ee902192e5
shravan.shan <shravan.shan@gmail.com>
2025-01-12 16:49:13 +0530

    linked in service comments

--- src/main/java/com/shravan/learn/linkedin/LinkedinMain.java
+++ src/main/java/com/shravan/learn/linkedin/LinkedinMain.java
@@ -1,5 +1,10 @@
 package com.shravan.learn.linkedin;
 
+// LinkedInService -> Users, JobPostings, Notifications
+// User -> name, Connections, Inbox messages, Sent message
+// Connection -> connection user, timestamp
+// Message -> send User, recipient User, content, timestamp
+// Notification -> NotificationType, recipient User, content, timestamp
 public class LinkedinMain {
     public static void main(String[] args) {
 
