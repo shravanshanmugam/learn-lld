b0e671a6e6e7d6c323d0aa7b2322d2d0295e1e71
shravan.shan <shravan.shan@gmail.com>
2025-01-12 15:35:24 +0530

    hotel management system comment

--- src/main/java/com/shravan/learn/hotelmanagementsystem/HotelManagementSystemMain.java
+++ src/main/java/com/shravan/learn/hotelmanagementsystem/HotelManagementSystemMain.java
@@ -1,5 +1,8 @@
 package com.shravan.learn.hotelmanagementsystem;
 
+
+// HMS -> Hotel, Reservation -> Room, Customer, checkIn Date checkout Date, status
+// Hotel -> Rooms by room type, Room -> room number, room type, status
 public class HotelManagementSystemMain {
     public static void main(String[] args) {
 
---
+++ src/main/java/com/shravan/learn/linkedin/LinkedinMain.java
@@ -0,0 +1,7 @@
+package com.shravan.learn.linkedin;
+
+public class LinkedinMain {
+    public static void main(String[] args) {
+
+    }
+}
