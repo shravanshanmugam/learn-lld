908080cf6535d88ff9597948e1ea877b45a53bc7
shravan.shan <shravan.shan@gmail.com>
2025-01-12 23:50:17 +0530

    wallet system comments

--- src/main/java/com/shravan/learn/wallet/WalletMain.java
+++ src/main/java/com/shravan/learn/wallet/WalletMain.java
@@ -4,15 +4,21 @@ import java.math.BigDecimal;
 
 // WalletSystem -> Users, Accounts, PaymentMethods,
 //                 createUser, getUser, createAccount, getAccount, addPaymentMethod, getPaymentMethod
-// User -> name, email, password, Accounts
+// User -> name, email, password, Accounts,
 // Account -> User, accountNumber, Currency, balance, Transactions, deposit, withdraw, addTransaction
-// Transaction -> source Account, destination Account, currency, amount, timestamp
+// Transaction -> source Account, destination Account, Currency, amount, timestamp
 // PaymentMethod (abstract) -> CreditCard, BankAccount
 public class WalletMain {
     public static void main(String[] args) {
 
     }
 
+    // A: INR 86 (USD 1), B: JPY 158 (USD 1)
+    // A transfer USD 1 to B
+    // convert USD 1 to INR and withdraw from A
+    // A: INR 0 (USD 0)
+    // convert USD 1 to JPY and deposit to B
+    // B: JPY 316 (USD 2)
     public void transferFunds(String sourceAccount, String destinationAccount, BigDecimal amount, Currency currency) {
         // if sourceAccount currency != currency -> convert amount to sourceAccount currency
         // sourceAccount.withdraw(amount)
